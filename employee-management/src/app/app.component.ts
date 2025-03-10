1c38f76072ee47d72ecd42ef0fa37a9342a7c3f4
optimal-Ops <nikitatosh@gmail.com>
2025-03-10 08:58:33 +0530

    update

---
+++ employee-management/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
+import { MasterComponent } from './components/master/master.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,MasterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'employee-management';
+}
